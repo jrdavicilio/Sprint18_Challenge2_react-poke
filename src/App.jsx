2c63bb9eadf45dcaba169bd9c903e1fc792a8965
David Fernández <davidferal26@gmail.com>
2025-07-12 23:29:22 +0200

    PROYECTO FINAL

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,70 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App () {
-  <></>
-};
+  const [namePokemon, setNamePokemon] = useState('')
+  const [resultPokemon, setResultPokemon] = useState(null)
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const getPokemon = async (pokemon) => {
+
+    try{
+      setIsLoading(true)
+      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      if(!res.ok) {
+        throw new Error(`Pokemon no encontrado ${res.status}`)
+      }
+
+      const data = await res.json()
+      setResultPokemon(data)
+
+    } catch (err) {
+      setError(err.message)
+      console.error(err)
+    } finally {
+      setIsLoading(false)
+    }
+
+  }
+
+  useEffect(() => {
+    const trimName = namePokemon.trim()
+    if(!trimName) {
+      setResultPokemon(null)
+      setError(null)
+      return
+    }
+
+    const delay = setTimeout (() => {
+      getPokemon(namePokemon)
+    }, 1000)
+
+    return () => clearTimeout(delay)
+
+  }, [namePokemon])
+
+
+
+
+  return (
+    <>
+    <input
+    type='text'
+    placeholder='nombre pokemon'
+    value={namePokemon}
+    onChange={e => setNamePokemon(e.target.value)}
+    />
+    {isLoading && <div className='spinner'></div>}
+    {error && <p>{error}</p>}
+
+    {resultPokemon && (
+      <>
+        <h2>{resultPokemon.name}</h2>
+        <img src={resultPokemon.sprites?.front_default} alt={resultPokemon.name} />
+      </>
+    )}
+    </>
+  )}
 
 export default App;
